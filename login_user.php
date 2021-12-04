5c6875c217a79c17c4f4d06f30cb4e0b020c7b37
LeeWeiYen7 <73536080+LeeWeiYen7@users.noreply.github.com>
2021-12-05 00:53:46 +0800

    Update login_user.php

--- login_user.php
+++ login_user.php
@@ -3,7 +3,7 @@ error_reporting(0);
 include_once("dbconnect.php");
 $email = $_POST['email'];
 $password = sha1($_POST['password']);
-$otp = $_POST['otp'];
+
 
 $sqllogin = "SELECT = FROM USER WHERE EMAIL = '$email' AND PASSWORD = '$password'AND OTP = '$otp'";
 $result = $conn-> query($sqllogin);
